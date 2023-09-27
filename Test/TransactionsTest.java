f0a2430b763b8ff92d5ffc356fb5877b45e6a4e8
KeaweA <keaweaquarian@gmail.com>
2023-09-26 20:42:40 -0400

    Tests compareTo

--- Test/TransactionsTest.java
+++ Test/TransactionsTest.java
@@ -9,11 +9,19 @@ public class TransactionsTest {
     Transactions transactions = new Transactions(new Account("Terry", new City("New York", "New York"), 'm'), LocalDate.now(),'D', 2.0);
 
     @Test
-    public void compareTo() {
+    public void compareToSameTransaction() {
+        assertEquals(0,transactions.compareTo(transactions));
+    }
+
+    @Test
+    public void compareToDifferentTransaction() {
+        Transactions transactions2 = new Transactions(new Account("Terry", new City("New York", "New York"), 'm'), LocalDate.now(),'D', 2.0);
+        assertEquals(-1,transactions.compareTo(transactions2));
     }
 
     @Test
     public void testToString() {
+        assertEquals("Transactions{trsNo=1, acc=10  Terry New York-New York m 0.0 null, date=2023-09-26, operation=D, amount=2.0}", transactions.toString());
     }
 
     @Test
