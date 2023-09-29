f06278407f0134e7a67792552a8e68cbf2778ad7
KeaweA <keaweaquarian@gmail.com>
2023-09-29 17:24:33 -0400

    Tests compareTo

--- Test/TransactionsTest.java
+++ Test/TransactionsTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 public class TransactionsTest {
     Transactions transactions = new Transactions(new Account("Terry", new City("New York", "New York"), 'm'), LocalDate.now(),'D', 2.0);
 //add differnet class
+    //Build a new project.
     @Test
     public void compareToSameTransaction() {
         assertEquals(0,transactions.compareTo(transactions));
